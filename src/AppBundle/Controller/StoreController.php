1e2e5adb8bd9aa69dfb1963e583ebc56c190b2c7
Nacer Naciri <93naciri@gmail.com>
2018-02-06 01:18:39 +0000

    Create sotre service and Controller

---
+++ src/AppBundle/Controller/StoreController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nacer
+ * Date: 05/02/18
+ * Time: 11:51
+ */
---
+++ src/AppBundle/Service/StoreRate.php
@@ -0,0 +1,67 @@
+<?php
+namespace AppBundle\Service;
+
+use AppBundle\Entity\Store;
+use AppBundle\Entity\User;
+use AppBundle\Entity\UserStore;
+use Doctrine\ORM\EntityManager;
+
+
+class StoreRate {
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+    public function listsStores(){
+        $stores = $this->em->getRepository(Store::class)->findAllStores();
+        return $stores;
+    }
+
+    public function likedStores(User $user){
+        $userStores = $this->em->getRepository(UserStore::class)->findStoresLiked($user);
+        return $userStores;
+    }
+
+    public function rateStore($action, Store $store,User $user){
+
+        $check = $this->checkIsRated($user, $store);
+
+        if(!$check){
+            $userStore = new UserStore();
+            $userStore->setRate($action);
+            $userStore->setUser($user);
+            $userStore->setStore($store);
+            $userStore->setCreatedAt(new \DateTime("now"));
+            $userStore->setUpdatedAt(new \DateTime("now"));
+
+
+
+            $this->em->persist($userStore);
+            $this->em->flush();
+        }
+        else{
+            if($check->getRate() == $action){
+                $this->em->remove($check);
+                $this->em->flush();
+            }
+            else{
+                $check->setRate($action);
+                $check->setUpdatedAt(new \DateTime("now"));
+                $this->em->persist($check);
+                $this->em->flush();
+            }
+        }
+    }
+
+    public function checkIsRated(User $user, Store $store){
+        $rate= $this->em->getRepository('AppBundle:UserStore')
+            ->findOneBy(['store' =>$store ,'user' => $user ]);
+
+        if ($rate){
+            return $rate;
+        }
+        else{
+            return null;
+        }
+    }
+}
