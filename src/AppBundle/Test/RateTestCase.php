bb2bba9dd82dd5d581977e7559a5057e9bce03f3
Nacer Naciri <93naciri@gmail.com>
2018-02-10 15:43:47 +0000

    Update Testing Classes RateTestCase and StoreControllerTest

--- src/AppBundle/Test/RateTestCase.php
+++ src/AppBundle/Test/RateTestCase.php
@@ -8,8 +8,29 @@
 
 namespace AppBundle\Test;
 
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
 
-class RateTestCase
+class RateTestCase extends TestCase
 {
+    private static $staticClient;
 
+    /**
+     * @vat Client
+     */
+    protected $client;
+
+    public static function setUpBeforeClass()
+    {
+        self::$staticClient= new Client([
+           'base_url' => 'http://localhost:8000',
+            'defaults'=> [
+                'exceptions' => false,
+            ]
+        ]);
+    }
+
+    public function setup(){
+        $this->client = self::$staticClient;
+    }
 }
--- tests/AppBundle/Controller/StoreControllerTest.php
+++ tests/AppBundle/Controller/StoreControllerTest.php
@@ -11,5 +11,4 @@ namespace Tests\AppBundle\Controller;
 
 class StoreControllerTest
 {
-
 }
